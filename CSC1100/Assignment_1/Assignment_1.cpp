37ba964af4a55afbc72ac88548705f26917418be
Quinn <quinnaho123@gmail.com>
2023-09-05 13:42:43 -0400

    Assignment 1

---
+++ CSC1100/Assignment_1/Assignment_1.cpp
@@ -0,0 +1,23 @@
+/*==========================================================
+
+ Title:       Assignment1 - First C++ Program
+ Course:      CSC 1100
+ Author:      <Quinn Aho>
+ Date:        <9/5/23>
+ Description: Output Practice
+
+ ==========================================================
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  //write your code here
+  cout << "Hello World!" << endl; //print Hello World!
+  cout << 100 + 1;                  //print the sum of 100 + 1
+  return 0;
+}
+
+
+
