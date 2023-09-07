8e72bb20f19b5bf9a6dcb6c9a3220940964edf56
Quinn <quinnaho123@gmail.com>
2023-09-06 21:53:58 -0400

    Assignment 2 & 3

---
+++ CSC1100/Assignment_3/Assignmet_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+float age;
+
+string dragonName;
+
+cout << age;
+
+float dragonAge = age * 10.5;
+
+cout << "The dragon named " << dragonName << " is " << dragonAge << " years old in wizard years." << endl;
+
+return 0;
+}
